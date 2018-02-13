73aa5c7187b3ab69797ac5fe076a6cdbbcb60c7a
gethin <gethin@yanxv.local>
2018-02-13 14:42:36 +0800

    modify 19

--- 19.py
+++ 19.py
@@ -6,15 +6,34 @@ class Solution:
     # matrix类型为二维列表，需要返回列表
     def printMatrix(self, matrix):
         # write code here
-        res = []
-        n = len(matrix)
-        m = len(matrix[0])
-        if n == 1 and m == 1:
-            res = [matrix[0][0]]
-            return res
-        for o in xrange((min(m, n) + 1) // 2):
-            [res.append(matrix[o][i]) for i in xrange(o, m - o)]
-            [res.append(matrix[j][m - 1 - o]) for j in xrange(o, n - o) if matrix[j][m - 1 - o] not in res]
-            [res.append(matrix[n - 1 - o][k]) for k in xrange(m -1 - o, o - 1, -1) if matrix[n - 1 - o][k] not in res]
-            [res.append(matrix[l][o]) for l in xrange(n - 1 - o, o - 1, -1) if matrix[l][o] not in res]
-        return res
+        lenRow = len(matrix)
+        lenCol = len(matrix[0])
+        print(lenRow, lenCol)
+        i, j, step = 0, 0, 1
+        startRow, startCol = 0, 0
+        result = []
+        while lenRow > 0 and lenCol > 0:
+            while (step == 1 and j < lenCol + startCol - 1) or (step == -1 and j > startCol):
+                result.append(matrix[i][j])
+                j += step
+            if step == -1:
+                startCol += 1
+            lenRow -= 1
+            while (step == 1 and i < lenRow + startRow) or (step == -1 and i > startRow + 1):
+                result.append(matrix[i][j])
+                i += step
+            if step == -1:
+                startRow += 1
+            lenCol -= 1
+            step = -step
+        result.append(matrix[i][j])
+        return result
+
+
+obj = Solution()
+print(obj.printMatrix([
+    [1, 2, 3, 4],
+    [5, 6, 7, 8],
+    [9, 10, 11, 12],
+    [13, 14, 15, 16],
+]))
