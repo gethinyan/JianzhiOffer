5673eab530935cf66b2e149dd5d532f0a7e7b552
gethin <gethin.yan@gmail.com>
2021-01-06 16:23:47 +0800

    数组中的逆序对

--- 30-39/35.py
+++ 30-39/35.py
@@ -3,14 +3,38 @@
 
 
 class Solution:
+    def __init__(self):
+        self.count = 0
+
     def InversePairs(self, data):
         # write code here
-        res = 0
-        for i in range(len(data)):
-            for j in range(i + 1, len(data)):
-                if data[i] > data[j]:
-                    res += 1
-        return res
+        if len(data) < 2:
+            return 0
+        self.mergeSort(data)
+        return self.count % 1000000007
+
+    def mergeSort(self, numList):
+        length = len(numList)
+        if length < 2:
+            return numList
+        middle = length // 2
+        leftList = self.mergeSort(numList[:middle])
+        rightList = self.mergeSort(numList[middle:])
+        leftLength = len(leftList)
+        rightLength = len(rightList)
+        mergedList = []
+        i, j = 0, 0
+        while i < leftLength and j < rightLength:
+            if leftList[i] < rightList[j]:
+                mergedList.append(leftList[i])
+                i += 1
+            # 当左边的大于右边的时候，表示左边（已排序）所有的元素都大于右边
+            else:
+                mergedList.append(rightList[j])
+                j += 1
+                self.count += leftLength - i
+
+        return mergedList + leftList[i:] + rightList[j:]
 
 
 obj = Solution()
